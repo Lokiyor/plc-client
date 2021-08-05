1c6ef67daa9b3cff74f8c2f7f23b08c6d1133ee0
lokiy <lujingyao@landleaf-tech.com>
2021-08-05 10:16:25 +0800

    初始化

---
+++ src/main/java/com/lokiy/iot/protocol/plc/client/PlcClientApplication.java
@@ -0,0 +1,13 @@
+package com.lokiy.iot.protocol.plc.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlcClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlcClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lokiy/iot/protocol/plc/client/PlcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokiy.iot.protocol.plc.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlcClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
